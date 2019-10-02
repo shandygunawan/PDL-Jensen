d59ef8ec6759920f4480f01c8e263ce747dfed7c
I Kadek Yuda Budipratama Giri <13516115@std.stei.itb.ac.id>
2019-10-03 00:28:28 +0700

    changed projection table name

--- projection_jansen.sql
+++ projection_jansen.sql
@@ -1,19 +1,19 @@
 CREATE OR REPLACE FUNCTION projection_jansen(_cols text)
 	RETURNS SETOF record AS $$
  	BEGIN
- 		RETURN QUERY EXECUTE 'SELECT ' || _cols || ', "Vs", "Ve", "T", "Op" FROM property_ownership';
+ 		RETURN QUERY EXECUTE 'SELECT ' || _cols || ', "vs", "ve", "t", "op" FROM property_ownership';
 	END;
 $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION projection_jansen_refcursor(query_name refcursor, _cols text)
 	RETURNS refcursor AS $$
  	BEGIN
- 		OPEN query_name FOR EXECUTE 'SELECT ' || _cols || ', "Vs", "Ve", "T", "Op" FROM property_ownership';
+ 		OPEN query_name FOR EXECUTE 'SELECT ' || _cols || ', "vs", "ve", "t", "op" FROM property_ownership';
 		return query_name;
 	END;
 $$ LANGUAGE plpgsql;
 
--- SELECT * FROM projection_jansen('customer_id') AS f(customer_id integer, "Vs" integer, "Ve" integer, "T" integer, "Op" "char");
+-- SELECT * FROM projection_jansen('customer_id') AS f(customer_id integer, "vs" integer, "ve" integer, "t" integer, "op" "char");
 
 -- SELECT projection_jansen_refcursor('ids', 'customer_id');
 -- FETCH ALL IN ids
