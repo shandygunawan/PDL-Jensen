0e87d82a953e5beb1a33d0246c131dc0de526a42
I Kadek Yuda Budipratama Giri <13516115@std.stei.itb.ac.id>
2019-10-03 00:27:20 +0700

    Fixed union

--- union_jansen.sql
+++ union_jansen.sql
@@ -1,7 +1,9 @@
-CREATE OR REPLACE FUNCTION union_jansen(_tbl1 anyelement, _tbl2 anyelement)
-	RETURNS SETOF anyelement AS $$
+CREATE OR REPLACE FUNCTION union_jansen(_tblname1 text, _tblname2 text)
+	RETURNS TABLE (customer_id int, customer_name text, property_number int, vs int, ve int, t int, op text) AS $$
 	
 	BEGIN
+		EXECUTE format('CREATE TEMP TABLE table1 ON COMMIT DROP AS SELECT * FROM %s', _tblname1);
+		EXECUTE format('CREATE TEMP TABLE table2 ON COMMIT DROP AS SELECT * FROM %s', _tblname2);
 		RETURN QUERY SELECT * FROM table1 UNION SELECT * FROM table2;
 	END;
 $$ language plpgsql;
